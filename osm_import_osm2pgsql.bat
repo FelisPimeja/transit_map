08345cfbf14a3f5a4ef1c5d05377d8ddb49bb152
Aleksandr Petrov <felispimeja@gmail.com>
2022-12-12 20:46:36 +0300

    Change osm file to import

--- osm_import_osm2pgsql.bat
+++ osm_import_osm2pgsql.bat
@@ -2,6 +2,6 @@
 FOR /F "eol=# tokens=*" %%i IN (%~dp0\.env) DO SET %%i
 
 @echo on
-@REM osm2pgsql -c -d %PGDB% -U %PGUSER% -H %PGHOST% -S %DIR%osm2pgsql.style %DATADIR%test1.osm
-osm2pgsql -c -d %PGDB% -U %PGUSER% -H %PGHOST% -O flex -S %DIR%routes.lua --hstore --multi-geometry %DATADIR%test1.osm
+@REM osm2pgsql -c -d %PGDB% -U %PGUSER% -H %PGHOST% -S %DIR%osm2pgsql.style %DATADIR%Moscow.osm.pbf
+osm2pgsql -c -d %PGDB% -U %PGUSER% -H %PGHOST% -O flex -S %DIR%routes.lua --hstore --multi-geometry %DATADIR%Moscow.osm.pbf
 @REM osm2pgsql --cache 1024 --number-processes 4 --verbose --create --database mc --output=flex --style bus-routes.lua --slim --flat-nodes nodes.cache --hstore --multi-geometry --drop
